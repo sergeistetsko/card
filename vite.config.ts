104a45d40a1624b501e859fb875a669f58cace40
sergeistetsko <sergeistetsko2021@yandex.by>
2025-11-07 21:13:53 +0300

    Add deploy settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,21 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
 
 
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: ['babel-plugin-styled-components'],
-      },
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@': '/src',
+    plugins: [
+        react({
+            babel: {
+                plugins: ['babel-plugin-styled-components'],
+            },
+        }),
+
+    ],
+    resolve: {
+        alias: {
+            '@': '/src',
+        },
     },
-  },
+    base: '/card/',
 });
 
